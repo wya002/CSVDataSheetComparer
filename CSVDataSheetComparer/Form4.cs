40f18efbedeb0c2f6f9d0581fb37cfef28caaa3a
우용안 <dndyddks001@naver.com>
2023-01-08 23:31:29 +0900

    feat : make chart (#3)

--- CSVDataSheetComparer/Form4.cs
+++ CSVDataSheetComparer/Form4.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,109 @@ namespace CSVDataSheetComparer
 {
     public partial class Form4 : Form
     {
-        public Form4()
+        public string attribute1, attribute2;
+        public Form4(string y1, string y2, List<string> y, List<string> compare_y, string range3, string range4, 
+            string range7, string range8, int rowcount1, int rowcount2, string filename1, string filename2)
         {
             InitializeComponent();
+            List<decimal> temp_y = new List<decimal>();
+            List<decimal> temp_compare_y = new List<decimal>();
+
+            for (int i = y.IndexOf(y1) + rowcount1; i <= y.Count - rowcount1 + y.IndexOf(y1); i += rowcount1)
+            {
+                try
+                {
+                    temp_y.Add(decimal.Parse(y[i]));
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            //double의 값을 넘어가는 값은 예외처리
+            for (int i = compare_y.IndexOf(y2) + rowcount2; i <= compare_y.Count - rowcount2 + compare_y.IndexOf(y2); i += rowcount2)
+            {
+                try
+                {
+                    temp_compare_y.Add(decimal.Parse(compare_y[i]));
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            for (int i = 0; i < temp_y.Count; i++)
+            {
+                try
+                {
+                    chart1.Series[0].Points.AddXY(i + 1, temp_y[i]);
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            for (int i = 0; i < temp_compare_y.Count; i++)
+            {
+                try
+                {
+                    chart1.Series[1].Points.AddXY(i + 1, temp_compare_y[i]);
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            chart1.Series[0].Name = filename1 + " - " + y1;
+            chart1.Series[1].Name = filename2 + " - " + y2;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_DoubleClick(object sender, EventArgs e)
+        {
+            ScreenCaptureForm(new Point(this.Left, this.Top), this.Size);
+        }
+
+        //차트 화면 캡처 함수
+        private void ScreenCaptureForm(Point _point, Size _size)
+        {
+            Rectangle rectangle = new Rectangle(_point, _size);
+
+            Bitmap bitmap = new Bitmap(rectangle.Width, rectangle.Height);
+            Graphics g = Graphics.FromImage(bitmap);
+
+            g.CopyFromScreen(rectangle.Left, rectangle.Top, 0, 0, rectangle.Size);
+
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Title = "Image Save";
+            dlg.DefaultExt = "png";
+            dlg.Filter = "png 파일(*.png)|*.png";
+            string fileName = "";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                fileName = dlg.FileName.ToString();
+                bitmap.Save(fileName, ImageFormat.Png);
+                bitmap.Dispose();
+            }
+            else
+            {
+                dlg.Dispose();
+                dlg = null;
+            }
         }
     }
 }
